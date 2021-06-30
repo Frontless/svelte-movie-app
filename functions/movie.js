ee01c4c11f6cb38cdd56d83e03c293cbeef11280
frontless <lequim12@gmail.com>
2021-06-30 23:52:28 +0900

    Create dotenv

--- functions/movie.js
+++ functions/movie.js
@@ -1,9 +1,9 @@
 const { default: axios } = require("axios");
+const { OMDB_API_KEY } = process.env
 
 exports.handler = async function(event, context){
     const params = JSON.parse(event.body)
     const { title, type, year, page, id } = params
-    const OMDB_API_KEY = "68c092d1";
     const url = id ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full` : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
 
     try{
